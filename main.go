a8096be550887c275bc7c69890d70f6caf86dac1
Matheus Simas <matheussimasalves@gmail.com>
2022-05-25 20:43:30 -0300

    como declarar uma struct e como preenchar(hard coded)

--- main.go
+++ main.go
@@ -2,6 +2,31 @@ package main
 
 import "fmt"
 
+type Pessoa struct {
+	Name string
+	Age  int8
+	Job  string
+}
+
+func printPessoa(pessoa Pessoa) {
+	fmt.Println("Nome: ", pessoa.Name)
+	fmt.Println("Idade: ", pessoa.Age)
+	fmt.Println("Prof: ", pessoa.Job)
+	fmt.Println("______________________")
+}
+
 func main() {
-	fmt.Println("tão então")
+	matheus := Pessoa{
+		Name: "Matheus",
+		Age:  28,
+		Job:  "Desenvolvedor",
+	}
+
+	var maria Pessoa
+
+	maria.Name = "Maria"
+	maria.Age = 8
+
+	printPessoa(matheus)
+	printPessoa(maria)
 }
