759d202989e963ac329cc8f15634574e7032d7f4
Matheus Simas <matheussimasalves@gmail.com>
2022-05-26 16:12:30 -0300

    Receiver com heranca

--- main.go
+++ main.go
@@ -1,33 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 type Circulo struct {
-	// nesse caso o raio está aberto somente para este package
-	raio float32
+	raio float64
 }
 
-// os metodos tem a funcao de atrelar funcoes a uma estrutura
-// o primeiro parametro preciso referenciar qual type (struct) estou usando.
-// no segundo são os paramentros de uma função comum
-func (c Circulo) MetodoSetarRaio(novoRaio float32) {
-	c.raio = novoRaio
-	fmt.Println(c.raio)
-}
+func (c *Circulo) Area() float64 {
+	return math.Pi * c.raio * c.raio
 
-// outra maneira de fazer isso é criando um "construtor"
-// mas aqui eu só crio uma função global para alterar o raio, sem atrelar a struct
-func NewRaio(r float32) Circulo {
-	return Circulo{raio: r}
+}
+func (c *Circulo) Diametro() float64 {
+	return 2 * math.Pi * c.raio
 }
 
 func main() {
-	// pra poder chamar o metodo, preciso definir uma variavel com a struct
-	circulo := Circulo{}
+	valorRaio := 5
+	calculaArea := Circulo{raio: float64(valorRaio)}
 
-	// depois consigo chamar meu método criado, referenciando o a variavel criada acima
-	circulo.MetodoSetarRaio(3.14)
-
-	//usando a função comum para obter a troca do raio
-	fmt.Println(NewRaio(10))
+	fmt.Println(calculaArea.Area())
 }
