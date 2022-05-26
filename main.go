0da6c13ca1b9c82af7a3e2719961f4624b3d314f
Matheus Simas <matheussimasalves@gmail.com>
2022-05-25 22:23:04 -0300

    Receiver / métodos / construtores

--- main.go
+++ main.go
@@ -1,33 +1,33 @@
 package main
 
-import (
-	"fmt"
-	"reflect"
-)
+import "fmt"
 
-type Pessoa struct {
-	// eu posso ler essa struct como json assim:
-	Name        string `json:"nome"`
-	Age         int8   `json:"idade"`
-	Job         string `json:"prof"`
-	Preferencia Preferencia
+type Circulo struct {
+	// nesse caso o raio está aberto somente para este package
+	raio float32
 }
 
-type Preferencia struct {
-	Comida string `json:"Comida"`
+// os metodos tem a funcao de atrelar funcoes a uma estrutura
+// o primeiro parametro preciso referenciar qual type (struct) estou usando.
+// no segundo são os paramentros de uma função comum
+func (c Circulo) MetodoSetarRaio(novoRaio float32) {
+	c.raio = novoRaio
+	fmt.Println(c.raio)
+}
+
+// outra maneira de fazer isso é criando um "construtor"
+// mas aqui eu só crio uma função global para alterar o raio, sem atrelar a struct
+func NewRaio(r float32) Circulo {
+	return Circulo{raio: r}
 }
 
 func main() {
-	pessoa := Pessoa{}
-	// o reflect traduz o tipo, o nome da s
-	p := reflect.TypeOf(pessoa)
+	// pra poder chamar o metodo, preciso definir uma variavel com a struct
+	circulo := Circulo{}
 
-	for i := 0; i < p.NumField(); i++ {
-		// o field traz o nome do campo do json
-		field := p.Field(i)
-		// tag traz o informações daqule campo na struct
-		tag := field.Tag.Get("json")
-		fmt.Println(tag)
-		fmt.Println(field)
-	}
+	// depois consigo chamar meu método criado, referenciando o a variavel criada acima
+	circulo.MetodoSetarRaio(3.14)
+
+	//usando a função comum para obter a troca do raio
+	fmt.Println(NewRaio(10))
 }
