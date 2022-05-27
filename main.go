22ce7e639a6c0b6a949853d6197ea027c3a1a1d9
Matheus Simas <matheussimasalves@gmail.com>
2022-05-27 19:13:08 -0300

    exerc 1 - file not writing correctly

--- main.go
+++ main.go
@@ -1,18 +1,46 @@
 package main
 
 import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"time"
+
 	"github.com/gin-gonic/gin"
 )
 
-func sayHello(c *gin.Context) {
+type Usuario struct {
+	Id          int       `json:"id"`
+	Nome        string    `json:"nome"`
+	Sobrenome   string    `json:"sobrenome"`
+	Email       string    `json:"email"`
+	Idade       int       `json:"idade"`
+	Altura      float32   `json:"altura"`
+	Ativo       bool      `json:"ativo"`
+	DataCriacao time.Time `json:"data_criacao"`
+}
+
+func writeJsonFile(c *gin.Context) {
+	file, _ := json.MarshalIndent(c, "", " ")
+	err := ioutil.WriteFile("/go-web/user.json", file, 0644)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+func hello(c *gin.Context) {
 	c.JSON(200, gin.H{
-		"message": "Hello!",
+		"message": "deuboa",
 	})
 }
 
 func main() {
+
 	router := gin.Default()
 
-	router.GET("/", sayHello)
+	router.GET("/", hello)
+	router.POST("/", writeJsonFile)
 	router.Run()
 }
